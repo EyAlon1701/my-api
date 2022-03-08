32bc31da513e5c3252dfcc70f50438777e000b2a
EyAlon1701 <101175557+EyAlon1701@users.noreply.github.com>
2022-03-08 13:49:16 +0200

    add Login function

--- controllers/accounts.js
+++ controllers/accounts.js
@@ -2,6 +2,24 @@ const express = require('express');
 
 const router = express.Router();
 
+const arr = [
+    {
+        username: 'aaa',
+        password: '111',
+        email: 'aaa@111'
+    },
+    {
+        username: 'bbb',
+        password: '222',
+        email: 'bbb@222'
+    },
+    {
+        username: 'ccc',
+        password: '333',
+        email: 'ccc@333'
+    }
+];
+
 router.get('/sayHello', (request,response) =>{
     return response.status(200).json({
         msg: 'Say hello from API route'
@@ -17,5 +35,23 @@ router.post('/sayHello', (request,response) =>{
         msg: `Hello ${fullName} from API route`
     });
 });
+
+router.post('/Login', (request,response) =>{
+
+    const {username,password,email} = request.body;
+    arr.forEach(user=>{
+        if(user.password==password && user.email==email)
+        {
+            return response.status(200).json({
+                msg: `Hello ${user.username} from API route`
+            });
+        }
+    }) 
+
+    return response.status(200).json({
+        msg: `u need to create a accounts`
+    });
+});
+
 
 module.exports = router;
